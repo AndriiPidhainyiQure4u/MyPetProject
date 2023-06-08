68dfe20a4c118579cd034d8f1bb8ae4ec778859b
Andrii <andrii.pidhainy.business@gmail.com>
2023-06-08 14:33:03 +0300

    add json class

--- app/tests/Controller/SubscribeControllerTest.php
+++ app/tests/Controller/SubscribeControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use App\Tests\AbstractControllerTest;
+use Modules\Auth\Test\Functional\Json;
 use Symfony\Component\HttpFoundation\Response;
 
 class SubscribeControllerTest extends AbstractControllerTest
@@ -19,7 +20,7 @@ class SubscribeControllerTest extends AbstractControllerTest
     {
         $content = json_encode(['email' => 'test@test.com']);
         $this->client->request('POST', '/api/v1/subscribe', [], [], [], $content);
-        $responseContent = json_decode($this->client->getResponse()->getContent());
+        $responseContent = Json::decode($this->client->getResponse()->getContent());
 
         self::assertResponseStatusCodeSame(Response::HTTP_BAD_REQUEST);
         self::assertJsonDocumentMatches($responseContent, [
