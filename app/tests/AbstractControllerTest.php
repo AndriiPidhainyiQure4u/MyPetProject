598083ea66f93c10abc9decd9091b8f82bab0592
Andrii <andrii.pidhainy.business@gmail.com>
2023-06-26 14:36:24 +0300

    add more test

--- app/tests/AbstractControllerTest.php
+++ app/tests/AbstractControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 use Helmich\JsonAssert\JsonAssertions;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -22,6 +23,55 @@ abstract class AbstractControllerTest extends WebTestCase
 
         $this->client = static::createClient();
         $this->em = self::getContainer()->get('doctrine.orm.entity_manager');
+        $this->hasher = self::getContainer()->get('security.user_password_hasher');
+    }
+
+    protected function auth(string $username, string $password): void
+    {
+        $this->client->request(
+            'POST',
+            '/api/v1/auth/login',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode(['username' => $username, 'password' => $password])
+        );
+
+        $this->assertResponseIsSuccessful();
+        $data = json_decode($this->client->getResponse()->getContent(), true, 512, JSON_THROW_ON_ERROR);
+
+        $this->client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data['token']));
+    }
+
+    protected function createUser(string $username, string $password): User
+    {
+        return $this->createUserWithRoles($username, $password, ['ROLE_USER']);
+    }
+
+    protected function createAdmin(string $username, string $password): User
+    {
+        return $this->createUserWithRoles($username, $password, ['ROLE_ADMIN']);
+    }
+
+    protected function createAuthor(string $username, string $password): User
+    {
+        return $this->createUserWithRoles($username, $password, ['ROLE_AUTHOR']);
+    }
+
+    private function createUserWithRoles(string $username, string $password, array $roles): User
+    {
+        $user = (new User())
+            ->setRoles($roles)
+            ->setLastName($username)
+            ->setFirstName($username)
+            ->setEmail($username);
+
+        $user->setPassword($this->hasher->hashPassword($user, $password));
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $user;
     }
 
     protected function tearDown(): void
